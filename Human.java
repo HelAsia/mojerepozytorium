c815d9e0323712729c688a94903855a3950b5e4a
HelAsia <asiaheluszka1993@gmail.com>
2017-07-24 21:44:50 +0200

    Dodano plik Human.java

---
+++ Human.java
@@ -0,0 +1,38 @@
+package cwiczenia;
+
+public class Human {
+	/*
+	int age;
+	int weight;
+	int height;
+	String name;
+	String male;
+	
+	public Human (int age, int weight, int height, String name, String male){
+		this.age = age;
+		this.weight = weight;
+		this.height = height;
+		this.name = name;
+		this.male = male;
+	}
+	*/
+	int getAge (int age){
+		return age;
+	}
+	
+	int getWeight (int weight){
+		return weight;
+	}
+
+	int getHeight (int height){
+		return height;
+	}
+	
+	String getName (String name){
+		return name;
+	}
+	
+	String isMale (String male){
+		return male;
+	}
+}
